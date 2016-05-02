54c8d99d70c647beda71152133261b1678ccf386
(Mirko) Emir Rodriguez <mirko@brightcrowns.com>
2016-05-02 18:25:20 -0300

    Drawing the City Map field

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.views.generic.base import TemplateView
 
 # My Imports
 from main.models import WDMCity, WDMCityMap, WDMCityMapItemType
+from main.engine import map_data, initializeRandomMap
 
 # Create your views here.
 
@@ -10,7 +11,9 @@ class IndexView(TemplateView):
     template_name = 'main.html'
 
     def get_context_data(self, **kwargs):
+        initializeRandomMap(40, 40)
         context = super(IndexView, self).get_context_data(**kwargs)
+        context['map_data'] = map_data
         return context
 
 
