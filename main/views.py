199f9acb73fdd394efadf858615182a7331f5fb0
(Mirko) Emir Rodriguez <mirko@brightcrowns.com>
2016-05-03 17:35:49 -0300

    Several functions, template for django. Created a new class to manage everything.

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.views.generic.base import TemplateView
 
 # My Imports
 from main.models import WDMCity, WDMCityMap, WDMCityMapItemType
-from main.engine import map_data, initializeRandomMap
+from main.engine import map_data, initializeRandomMap, UserCityMap
 
 # Create your views here.
 
@@ -11,10 +11,13 @@ class IndexView(TemplateView):
     template_name = 'main.html'
 
     def get_context_data(self, **kwargs):
-        initializeRandomMap(40, 40)
+        initializeRandomMap(20, 20)
+        user_map = UserCityMap(20, 20)
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['map_data'] = map_data
+        context['map_data'] = [[y for y in x.itervalues()] for x in map_data.itervalues()]
+        context['user_map'] = user_map.citymap
+        context['user_map_inf'] = user_map.citymapinf
+        import ipdb; ipdb.set_trace()
         return context
-
 
 index = IndexView.as_view()
