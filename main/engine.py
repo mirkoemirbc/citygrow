8e599143edb2b3ef7933578fe3ffdcccdd2a5160
(Mirko) Emir Rodriguez <mirko@brightcrowns.com>
2016-05-04 21:23:45 -0300

    Created a new class for tiles. Create new method for class UserCityMap to raise the population.

--- main/engine.py
+++ main/engine.py
@@ -16,6 +16,13 @@ map_tiles_value = (('00000', 0.5), ('00003', 0.5), ('00011', 0.5),
 
 map_tiles_center = ('00091', 10.0)
 
+
+def percent(num1, num2):
+    """ Return the percent """
+    return round(num1 / num2 * 100, 2)
+
+
+# ALL THIS IS DEPRECATED ------------------------------ #
 # Define array for data saving
 map_data = defaultdict(lambda: defaultdict(int))
 map_growing_rate = defaultdict(lambda: defaultdict(int))
@@ -74,17 +81,42 @@ def evaluateCityGrow(x, y):
 #  CLASS DEFINITION                                                #
 # **************************************************************** #
 
+class CityMapTile:
+    """ The tile information """
+
+    def __init__(self, x, y, xorig=0, yorig=0, mapid=0):
+        self.x = x
+        self.y = y
+        self.xorig = xorig
+        self.yorig = yorig
+        self.available = 1
+        self.type = '00000'
+        self.hp = 0
+        self.hp_total = 0
+        self.influence = 1
+        self.population = 0
+
+    def movepopulation(self, population):
+        self.population = population
+        return population
+
+    def moveinfluence(self, inf):
+        self.influence = inf
+        return inf
+
+
 class UserCityMap:
     """ The City Map class """
 
-    def __init__(self, xmax, ymax):
+    def __init__(self, xmax, ymax, playerid=0):
         self.xmax = xmax
         self.ymax = ymax
         self.citymap = []
         self.citymapinf = []
-        self.player = 0
+        self.player = playerid
         self.xcenter = 0
         self.ycenter = 0
+        self.population = []
         self.initcitymap()
 
     def initcitymap(self):
@@ -92,12 +124,15 @@ class UserCityMap:
         for axis_x in range(self.xmax):
             col = []
             inf = []
+            pop = []
             for axis_y in range(self.ymax):
                 col.append(random.choice(list_map_tiles))
                 inf.append(1.0)
+                pop.append(0)
 
             self.citymapinf.append(inf)
             self.citymap.append(col)
+            self.population.append(pop)
 
         # Define the City Center spot
         self.xcenter = int(self.xmax / 2) + random.randint(-2, 2)
@@ -138,6 +173,18 @@ class UserCityMap:
         # TODO: recorrer los edificios cercanos al punto y sumar habitantes,
         # si ha llegado al 80% de la influencia total. Luego marcar como
         # "no cambiante".
-        # TODO: Clase tile con una matriz de 5x5
-        # TODO: Recorrer los tiles de la zona de influencia, desmarcando los
-        # que eran "no cambiante"
+
+    def citygrowinhabitant(self, x, y):
+        """ Inspect all building beside the point and raise the resident number """
+        for axis_x in range(x - 1 if x - 1 >= 0 else 0,
+                            x + 1 if x + 1 <= self.xmax else self.xmax):
+            for axis_y in range(y - 1 if y - 1 >= 0 else 0,
+                                y + 1 if y + 1 <= self.ymax else self.ymax):
+                if axis_x != x and axis_y != y:
+                    if percent(self.citymapinf[axis_x][axis_y],
+                               self.citymapinf[x][y]) > 75:
+                        self.population[axis_x][axis_y] += 1
+
+    # TODO: Clase tile con una matriz de 5x5
+    # TODO: Recorrer los tiles de la zona de influencia, desmarcando los
+    # que eran "no cambiante"
